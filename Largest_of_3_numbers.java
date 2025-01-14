98da99a5e3b1803078f0ac791da65273039fd3e8
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:10:11 +0530

    13) Largest of 3 numbers

---
+++ Largest_of_3_numbers.java
@@ -0,0 +1,28 @@
+//13 Largest of 3 numbers:
+package control_statements;
+
+public class Assign1 {
+public static void main(String[] args)
+{
+	int a=100;
+	int b=2000;
+	int c=30;
+	
+	if(a>b && a>c)
+	{
+		System.out.println("a is greater");
+		
+	}
+	else if(b>a && b>c)
+	{
+		System.out.println("b is greater");
+		
+	}
+	else
+	{
+		System.out.println("c is greater");
+		
+	}
+}
+}
+
