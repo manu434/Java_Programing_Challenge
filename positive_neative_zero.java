6f4af744330407b94c2800e3faf37ca9f894d231
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:02:24 +0530

    9) Check number (+,-,0)

---
+++ positive_neative_zero.java
@@ -0,0 +1,23 @@
+//9. Check if a Number is Positive, Negative, or Zero
+
+package assigments;
+public class Practice {
+public static void main(String[] args) {
+
+	int a =-99;
+	if (a<0)
+	{
+		System.out.println("The number is Negative");
+		
+	}
+	else if(a>0)
+	{
+		System.out.println("The number is postive");
+		
+	}
+	else
+	{
+		System.out.println("The number is zero");
+	}
+ }
+}
