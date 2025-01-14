22171277388ec59a2570c30f3b3229d5482c2041
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:11:27 +0530

    14) Area of Rectangle

---
+++ Area_Rectangle.java
@@ -0,0 +1,13 @@
+//14 Area of Rectangle:
+package basics;
+
+public class Area_rectangle {
+public static void main(String[] args)
+{
+	int length=10;
+	int width=20;
+	int Area=length*width;
+	System.out.println("The Are of rectangle is:" + Area);
+}
+}
+
