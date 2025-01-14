64420c1b22349793f068c33435ab37f707d9d836
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:46:07 +0530

    20) Convert string to integr

---
+++ Convert_String_to_int.java
@@ -0,0 +1,19 @@
+//20 Java program to convert a String to integer
+
+package assigments;
+public class Practice {
+public static void main(String[] args) {
+
+	//check wheather it is adding
+	String str= "9";
+	System.out.println(str+1);
+	
+	//convert
+	int number =Integer.parseInt(str);
+	System.out.println(number);
+	
+	//check wheather it is adding
+	System.out.println(20+number);
+	
+}
+}
