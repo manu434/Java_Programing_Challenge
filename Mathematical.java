ec0cb7b7ede4e65f77ef7f819c6349a93f0897bc
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:01:10 +0530

    8) Mathematical Solve

---
+++ Mathematical.java
@@ -0,0 +1,10 @@
+
+//8 Java program - What is the output for System.out.println(7+(3*5)*(4/2)+(5%3)-1);
+package assigments;
+public class Practice {
+public static void main(String[] args) {
+	
+    System.out.println(7+(3*5)*(4/2)+(5%3)-1);
+ }
+}
+
