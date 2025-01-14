34e17d4fb3c5185f3c8e3acd0d8b142c1783d897
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 20:58:04 +0530

    5) variable is Odd or even

---
+++ num_odd.java
@@ -0,0 +1,18 @@
+//5 find the given number is odd or even
+package assigments;
+import java.util.Scanner;
+
+public class Practice {
+public static void main(String[] args) {
+	
+	
+	int num1 =10;
+	
+	if(num1 %2 == 0){
+		System.out.println(num1 +  "The number is Even:");
+	}else {
+			System.out.println(num1+ "The number is odd:");
+			}
+		
+}
+}
