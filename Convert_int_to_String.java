e8c1ca243916b2b8b7db37a1b566c5d5f0986032
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:51:06 +0530

    21) Convert int to string

---
+++ Convert_int_to_String.java
@@ -0,0 +1,20 @@
+//21 java program convert integer to string:
+
+package assigments;
+public class Practice {
+public static void main(String[] args) {
+
+	//check wheather it is adding
+	int number= 9;
+	System.out.println(number+19);
+	
+	//convert
+	String str =Integer.toString(number);
+	System.out.println(str);
+	
+	//check wheather it is adding
+	System.out.println(20+str);
+	
+}
+}
+
