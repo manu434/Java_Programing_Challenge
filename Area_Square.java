2d94085bb5e360a03bddd93c8471c00f375d5297
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:12:52 +0530

    15) Area of Square

---
+++ Area_Square.java
@@ -0,0 +1,13 @@
+
+//15 Area of Square:
+package basics;
+
+public class Area_square {
+public static void main(String[] agrs)
+{
+	int square=20;
+	int Area=square*square;
+	System.out.println("The Are of square is:" + Area);
+	
+}
+}
