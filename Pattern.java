db80e711975301598b44b603da7863288eb778fa
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:17:33 +0530

    17) Pattern Drawing

---
+++ Pattern.java
@@ -0,0 +1,19 @@
+//17: Shapes with Symbols
+//Write a program to display simple shapes using symbols and println.
+
+package assigments;
+public class Personal{
+	public static void main(String[] agrs) {
+		//Square
+		System.out.println("****");
+		System.out.println("*  *");
+		System.out.println("*  *");
+		System.out.println("*  *");
+		System.out.println("*  *");
+		System.out.println("****");
+       //Rectangle
+		System.out.println(" * ");
+		System.out.println("***");
+		System.out.println("****");
+	}
+}
