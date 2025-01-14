a84ce0b8a78303161648192e1ebf0001aec616b4
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:37:58 +0530

    19) Sum of first 100 numbers

---
+++ Sum_of_first_100.java
@@ -0,0 +1,15 @@
+//19 Java program to find the sum of first 100 numbers
+
+package assigments;
+public class Practice {
+public static void main(String[] args) {
+
+	int sum=0;
+	for (int i=0;i<=100;i++)
+	{
+		sum=sum+i;
+		}
+	System.out.println(sum);
+   }
+}
+
