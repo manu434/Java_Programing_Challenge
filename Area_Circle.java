ea2c5504766d330b12cbe423e3bb221335caf286
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-14 21:13:52 +0530

    16) Area of Circle

---
+++ Area_Circle.java
@@ -0,0 +1,15 @@
+//16 Area of Circle:
+package basics;
+
+public class Area_circle {
+public static void main(String[] args)
+{
+ 
+ int radius=5;
+ double Area=Math.PI*radius*radius;
+ System.out.println("Area of Circle is:" + Area);
+ 
+}
+}
+
+
