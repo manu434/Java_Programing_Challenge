7577c80d7173b739c20a3636154c06b14a3b1e30
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-16 21:37:24 +0530

    29) print 1 to 100

---
+++ 1_to_100.java
@@ -0,0 +1,11 @@
+//29 print 1 to 100 using for loop:
+package control_statements;
+
+public class For_loop {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
+
