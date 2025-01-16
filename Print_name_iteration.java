f22387996eac91efae1f41112edb95b9716b500b
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-16 21:40:49 +0530

    31) Print name 10 times

---
+++ Print_name_iteration.java
@@ -0,0 +1,13 @@
+//31: Print name in 10 times
+
+package control_statements;
+
+public class For_loop {
+	public static void main(String[] args) {
+		String name = "manu";
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(name);
+		}
+	}
+}
