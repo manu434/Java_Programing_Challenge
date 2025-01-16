190e5b77e70f20c7b7f8078937ea20f116533111
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-16 21:39:09 +0530

    30) 10 to 1 print

---
+++ 10_to_1.java
@@ -0,0 +1,12 @@
+
+//30  print 10 to 1:
+
+package control_statements;
+
+public class For_loop {
+	public static void main(String[] args) {
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+	}
+}
