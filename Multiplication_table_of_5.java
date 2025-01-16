3565337d8f870c640a59f37415db4452cddd1a77
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-16 21:44:01 +0530

    33) Multiplication table of given number

---
+++ Multiplication_table_of_5.java
@@ -0,0 +1,16 @@
+
+//33: Multiplication table of no  5:
+package control_statements;
+
+public class For_loop {
+	public static void main(String[] args) {
+		int num = 5;
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.println(num + "x" + i + "=" + (num * i));
+		}
+	}
+}
+
+
