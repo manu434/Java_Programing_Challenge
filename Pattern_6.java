d500fdbad25bc1ed71a655aa3bf664008a89a876
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-19 21:42:15 +0530

    40) Pattern_6

---
+++ Pattern_6.java
@@ -0,0 +1,12 @@
+//40:inverted Right_angled Triangle:
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
