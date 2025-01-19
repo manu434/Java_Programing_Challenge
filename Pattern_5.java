2d142a7d3d57660b70a52cad1c267ee5afa1b5c1
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-19 21:39:42 +0530

    39) Pattern_5

---
+++ Pattern_5.java
@@ -0,0 +1,21 @@
+//39://Q5: print  11
+//////////// 12
+//////////// 13
+///////////  21
+//////////   22
+//////////   23
+
+package control_statements;
+
+public class Nested_loop {
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 3; i++) {
+			for (int j = 1; j <= 3; j++) {
+				System.out.println(i + " " + j);
+
+			}
+
+		}
+	}
+}
