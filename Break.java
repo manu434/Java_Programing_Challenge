61a3457eb04a5bf9659bf5169b41b927ceb0baea
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-20 20:58:57 +0530

    41) Break program

---
+++ Break.java
@@ -0,0 +1,14 @@
+/41:Break
+package control_statements;
+
+public class Break {
+	public static void main(String[] args) {
+		for (int i = 0; i <= 10; i++) {
+			if (i == 4) {
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+}
+
