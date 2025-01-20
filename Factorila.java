808b0633ef0b0ea1d5d5d40e6dd472dec9403e71
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-20 22:53:58 +0530

    46) Factorial

---
+++ Factorila.java
@@ -0,0 +1,14 @@
+//46: Factorial of a number:
+
+package assigments;
+
+public class Practice {
+	public static void main(String[] args) {
+		int f = 1;
+		for (int i = 1; i <= 5; i++) {
+			f = f * i;
+
+		}
+		System.out.println(f);
+	}
+}
