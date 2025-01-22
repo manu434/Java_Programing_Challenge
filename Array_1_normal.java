bd4ede71301e4fdda6b3a76fc4f5bc271eecb92e
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-22 19:33:17 +0530

    50) Array pgm Normal

---
+++ Array_1_normal.java
@@ -0,0 +1,20 @@
+//50: Array program//1:normal array declaration in java 1-dimensional:
+package array;
+
+public class Array1d {
+	public static void main(String[] args) {
+
+		int[] a = new int[4]; // if we don't know the value we need to input the values;
+
+		a[0] = 11;
+		a[2] = 12;
+		a[1] = 40;
+		a[3] = 55;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+
+	}
+}
+
