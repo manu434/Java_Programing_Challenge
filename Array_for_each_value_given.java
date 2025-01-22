913ae791546e5b90b959aa15d9ea4251427f752d
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-22 19:45:28 +0530

    56) Array_for_each_value_given

---
+++ Array_for_each_value_given.java
@@ -0,0 +1,12 @@
+//56:Array pgm with values already given:
+package array;
+
+public class Array1d {
+	public static void main(String[] args) {
+		int[] a = { 12, 23, 23, 44, 55, 66 };
+		System.out.println("print the elements:");
+		for (int v : a) {
+			System.out.println(v);
+		}
+	}
+}
