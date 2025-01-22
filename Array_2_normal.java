3d2137a55187c06df4ccc2726fe418ed5cbc9481
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-22 19:34:41 +0530

    51) Array pgm Normal_2

---
+++ Array_2_normal.java
@@ -0,0 +1,15 @@
+//51:Arrag pgm////2: normal array declaration in java 1-dimensional:
+package array;
+
+public class Array1d {
+	public static void main(String[] args) {
+
+		int[] a = { 11, 12, 40, 55 };// if we know the values;
+
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+
+	}
+}
