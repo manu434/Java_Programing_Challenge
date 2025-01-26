8cc8ca098cdb36b17e52ed47f35f6231ff58680b
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-26 16:48:47 +0530

    62) Check String contains a string

---
+++ Contains.java
@@ -0,0 +1,18 @@
+//62://Q2:Input a string and find the given string is present or not
+package stringMethods;
+
+public class Assign {
+	public static void main(String[] args) {
+
+		String s1 = "java language is very powerful";
+
+		if (s1.contains("language")) {
+			System.out.println("String is present:");
+
+		} else {
+			System.out.println("not present in the string:");
+		}
+
+	}
+
+}
