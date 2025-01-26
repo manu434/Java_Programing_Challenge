22596f9e9974dd3965321fefc5ed425b62cce894
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-26 16:47:04 +0530

    61) Concatenate of string

---
+++ Concatenate.java
@@ -0,0 +1,12 @@
+//61: concatentae the string
+Q1:cocatenate:
+
+package stringMethods;
+
+public class Assign {
+	public static void main(String[] args) {
+		String s = "jmeter";
+		String s2 = "Performance testing tool";
+		System.out.println(s.concat(" " + s2 + " "));
+	}
+}
