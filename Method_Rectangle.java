2d60a3c711a7a37ae606bbbb0317a726d6964691
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-28 23:21:11 +0530

    87) Method_Rectangle

---
+++ Method_Rectangle.java
@@ -0,0 +1,17 @@
+//87)	package Oops;
+
+//with parameters and with return type:
+public class Rectangle {
+	public int area(int a, int b) {
+		int z = a * b;
+		return z;
+	}
+
+	public static void main(String[] args) {
+		Rectangle sc = new Rectangle();
+		System.out.println(sc.area(2, 3));
+
+	}
+
+}
+
