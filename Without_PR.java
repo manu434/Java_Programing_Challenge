bb1ff469cba85801e9a20408fe9179a401f8a68f
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-28 23:17:05 +0530

    83) Withour_PR

---
+++ Without_PR.java
@@ -0,0 +1,22 @@
+//83)	package Oops;
+// without parameter && without return type:
+
+//here the execution will be normal method programming and creating object in main method and accessing method name using that
+// No Parameter and No Return type
+
+public class WIthout_PR {
+
+	public void add() {
+		int a = 4, b = 10;
+		int c = a + b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		WIthout_PR sc = new WIthout_PR();
+		sc.add();
+
+	}
+
+}
+
