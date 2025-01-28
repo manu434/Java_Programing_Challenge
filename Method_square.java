a1bd8988e3ee691b787e57dfbd557d3f66ae3f2d
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-28 23:23:12 +0530

    89) Method_square

---
+++ Method_square.java
@@ -0,0 +1,18 @@
+//89)	package Oops;
+
+//without parameter and with return type:
+public class Square {
+	public int area() {
+		int a = 5;
+		int z = a * a;
+		return z;
+	}
+
+	public static void main(String[] args) {
+		Square sc = new Square();
+		System.out.println(sc.area());
+
+	}
+
+}
+
