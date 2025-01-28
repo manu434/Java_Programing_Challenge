6ab181012824703e5b40a823764e972d869717ef
MANU K SHIBU <76098146+manu434@users.noreply.github.com>
2025-01-28 23:19:53 +0530

    86) With_P_Without_R

---
+++ With_P_Without_R.java
@@ -0,0 +1,17 @@
+//86)	package Oops;
+
+public class With_P_Without_R {
+//With parameter and without Return type:
+	public void mul(int a, int b) {
+		int c = a * b;
+		System.out.println(c);
+
+	}
+
+	public static void main(String[] args) {
+		With_P_Without_R sc = new With_P_Without_R();
+		sc.mul(20, 2);
+
+	}
+}
+
